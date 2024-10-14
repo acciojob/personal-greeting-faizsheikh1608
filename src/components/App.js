252f84d4d358212125e8ef0e2bdc6e43b821b790
Faiz <sheikhfaiz7861@gmail.com>
2024-10-14 18:39:05 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [userInput, setUserInput] = useState("");
+  const handleOnchange = function (e) {
+    setUserInput(e.target.value);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Enter your name:</h1>
+      <input type="text" onChange={(e) => handleOnchange(e)}></input>
+      <h1>{userInput !== "" && `Hello ${userInput}!`}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
